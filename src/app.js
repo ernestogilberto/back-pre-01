3886155f4cdb48ac8d79cf2fd650587bdbe2944a
Ernesto Gilberto Valenzuela Gallardo <ernestogilberto@hotmail.com>
2024-01-23 20:09:06 -0600

    añadiendo el proyecto.

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import {router as productsRouter} from "./routes/products.js"
+import {router as cartsRouter} from "./routes/carts.js"
+
+
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{res.send ({message: "Bienvenido al servidor express", endpoints: {'products': '/api/products', 'carts': '/api/carts'}})})
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
+
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`)
+})
